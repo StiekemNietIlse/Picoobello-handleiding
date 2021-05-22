36dd3e1f541d53e611018377a45a9fdc2050a9a0
ilse <ilsenijhuis99@gmail.com>
2021-05-22 21:49:30 +0200

    meerdere schermen toegevoegd en kan varianbele meegeven in de button om door te gaan daar volgend scherm

--- App.js
+++ App.js
@@ -1,16 +1,61 @@
-import React from 'react';
-import { Text, View } from 'react-native';
+import 'react-native-gesture-handler';
+import * as React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+import {Button, Text} from 'react-native';
+const Stack = createStackNavigator();
 
-const HelloWorldApp = () => {
+const App = () => {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center"
-      }}>
-      <Text>Hello, world!</Text>
-    </View>
-  )
-}
-export default HelloWorldApp;
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{title: 'Welcome'}}
+        />
+        <Stack.Screen name="Profile" component={ProfileScreen} />
+        <Stack.Screen name="Profile2" component={ProfileScreen} />
+        <Stack.Screen name="Profile3" component={ProfileScreen} />
+        <Stack.Screen name="Test" component={TestScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+const HomeScreen = ({navigation}) => {
+  return (
+    <>
+      <Button
+        title="Go to Jane's profile"
+        onPress={() =>
+          navigation.navigate('Profile', {
+            text: '1',
+            link: 'Profile2',
+          })
+        }
+      />
+      <Text>test</Text>
+    </>
+  );
+};
+const ProfileScreen = ({navigation, route}) => {
+  
+  return (
+    <>
+      <Button
+        title={route.params.link.toString()}
+        onPress={() =>
+          navigation.navigate(route.params.link, {
+            text: 'hgfhgjfjffggfisgggggggggggggggggdgddddddddddddddddddddddd',
+            link: 'Profile3',
+          })
+        }
+      />
+      <Text>{route.params.text}</Text>
+    </>
+  );
+};
+const TestScreen = ({navigation, route}) => {
+  return <Text>Test value is: {route.params.test}</Text>;
+};
+export default App;
