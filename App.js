fe0a6aee83222918b01125b2992ae6472362f71c
ilse <ilsenijhuis99@gmail.com>
2021-06-14 23:17:06 +0200

    knop aangepast zodat die constant op de laatste pagina wordt getoont van JSONfile + design aangepast

--- App.js
+++ App.js
@@ -25,112 +25,109 @@ const App = () => {
       <Stack.Navigator>
         <Stack.Screen name="HomeScreen" component={HomeScreen} />
         <Stack.Screen name="Home" component={ContinuesPage} />
-        <Stack.Screen name="Test" component={TestScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
 };
 
 const ContinuesPage = ({navigation, route}) => {
-  console.log(route.params);
-  if (route.params) {
-    if (route.params.number) {
-      let index = pages[route.params.number - 1].toString().split('/');
-      let lastPage = false;
-      if (index[0] === index[1]) {
-        lastPage = true;
-      }
-      return (
-        <ImageBackground
-          source={require('./images/Background.png')}
-          style={{
-            flex: 1,
-            justifyContent: 'center',
-            alignItems: 'center',
-            resizeMode: 'cover',
-            width: SCREEN_WIDTH,
-            height: SCREEN_HEIGHT,
-          }}>
-          <View>
-            <Text
-              style={{textAlign: 'center', marginBottom: '5%', fontSize: 25}}>
-              {pages[route.params.number - 1].name}
-            </Text>
-            <Image
-              style={{width: 700, height: 700, resizeMode: 'contain'}}
-              source={pages[route.params.number - 1].imagePath}
-            />
-            <Text style={{textAlign: 'center', marginBottom: '5%'}}>
-              {pages[route.params.number - 1].text}
-            </Text>
-            {pages[route.params.number - 1].information == undefined ? (
-              <Text />
-            ) : (
-              <Text
-                style={{
-                  textAlign: 'center',
-                  color: '#292D78',
-                  marginBottom: '5%',
-                  backgroundColor: '#C8EBFB',
-                }}>
-                {pages[route.params.number - 1].information}
-              </Text>
-            )}
-            <Text style={{textAlign: 'center', marginBottom: '5%'}}>
-              {pages[route.params.number - 1].question}
-            </Text>
-          </View>
-          {route.params.number - 1 == '6' ? (
-            <TouchableOpacity
-              onPress={() =>
-                navigation.navigate('HomeScreen', {
-                  number: 0,
-                })
-              }
-              style={{
-                marginTop: '10%',
-                backgroundColor: 'blue',
-                width: '50%',
-                padding: '2%',
-                borderRadius: 50,
-              }}>
-              <Text
-                style={{
-                  textAlign: 'center',
-                  color: 'white',
-                  fontWeight: '800',
-                }}>
-                Ja, ga verder
-              </Text>
-            </TouchableOpacity>
-          ) : (
-            <TouchableOpacity
-              onPress={() =>
-                navigation.navigate('Home', {
-                  number: route.params.number + 1,
-                })
-              }
-              style={{
-                marginTop: '10%',
-                backgroundColor: 'blue',
-                width: '50%',
-                padding: '2%',
-                borderRadius: 50,
-              }}>
-              <Text
-                style={{
-                  textAlign: 'center',
-                  color: 'white',
-                  fontWeight: '800',
-                }}>
-                Ja, ga verder
-              </Text>
-            </TouchableOpacity>
-          )}
-        </ImageBackground>
-      );
-    }
+  console.log(pages[route.params.number - 1].name);
+  const index = pages[route.params.number - 1].name.split('/');
+  let lastPage = false;
+  if (index[0] === index[1]) {
+    lastPage = true;
   }
+  console.log(index[0]);
+  console.log(index[1]);
+  return (
+    <ImageBackground
+      source={require('./images/Background.png')}
+      style={{
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        resizeMode: 'cover',
+        width: SCREEN_WIDTH,
+        height: SCREEN_HEIGHT,
+      }}>
+      <View>
+        <Text style={{textAlign: 'center', marginBottom: '2%', fontSize: 22}}>
+          {pages[route.params.number - 1].name}
+        </Text>
+        <Text style={{textAlign: 'center', marginBottom: '2%', fontSize: 22}}>
+          {pages[route.params.number - 1].text}
+        </Text>
+        <Image
+          style={{width: 800, height: 800, resizeMode: 'contain'}}
+          source={pages[route.params.number - 1].imagePath}
+        />
+        {pages[route.params.number - 1].information == undefined ? (
+          <Text />
+        ) : (
+          <Text
+            style={{
+              textAlign: 'center',
+              color: '#292D78',
+              margin: '2%',
+              backgroundColor: '#C8EBFB',
+              fontSize: 22,
+            }}>
+            {pages[route.params.number - 1].information}
+          </Text>
+        )}
+        <Text style={{textAlign: 'center', marginBottom: '2%', fontSize: 22}}>
+          {pages[route.params.number - 1].question}
+        </Text>
+      </View>
+      {route.params.number - 1 == index[1] ? (
+        <TouchableOpacity
+          onPress={() =>
+            navigation.navigate('HomeScreen', {
+              number: 0,
+            })
+          }
+          style={{
+            marginTop: '10%',
+            backgroundColor: 'blue',
+            width: '50%',
+            padding: '2%',
+            borderRadius: 50,
+          }}>
+          <Text
+            style={{
+              textAlign: 'center',
+              color: 'white',
+              fontSize: 22,
+            }}>
+            Ja, ga verder
+          </Text>
+        </TouchableOpacity>
+      ) : (
+        <TouchableOpacity
+          onPress={() =>
+            navigation.navigate('Home', {
+              number: route.params.number + 1,
+            })
+          }
+          style={{
+            marginTop: '10%',
+            backgroundColor: '#F1A948',
+            width: '50%',
+            padding: '2%',
+            borderRadius: 50,
+          }}>
+          <Text
+            style={{
+              textAlign: 'center',
+              color: 'white',
+              fontSize: 22,
+            }}>
+            Ja, ga verder
+          </Text>
+        </TouchableOpacity>
+      )}
+    </ImageBackground>
+  );
 };
 
 const HomeScreen = ({navigation}) => {
@@ -155,13 +152,20 @@ const HomeScreen = ({navigation}) => {
           <Text
             style={{
               textAlign: 'center',
-              fontSize: 30,
+              fontSize: 22,
               marginBottom: '5%',
               color: '#292D78',
             }}>
             {pages[0].text}
           </Text>
-          <Text>{pages[0].question}</Text>
+          <Text
+            style={{
+              textAlign: 'center',
+              fontSize: 22,
+              color: '#292D78',
+            }}>
+            {pages[0].question}
+          </Text>
         </View>
         <TouchableOpacity
           onPress={() =>
@@ -171,15 +175,15 @@ const HomeScreen = ({navigation}) => {
           }
           style={{
             marginTop: '10%',
-            backgroundColor: 'blue',
-            width: '50%',
+            backgroundColor: '##F1A948',
+            width: '70%',
             padding: '2%',
             borderRadius: 50,
           }}>
           <Text
             style={{
               textAlign: 'center',
-              fontWeight: '800',
+              fontSize: 22,
               color: 'white',
             }}>
             Picoo opstarten
@@ -188,8 +192,5 @@ const HomeScreen = ({navigation}) => {
       </ImageBackground>
     </>
   );
-};
-const TestScreen = ({navigation, route}) => {
-  return <Text>Test value is: {route.params.test}</Text>;
 };
 export default App;
--- json/pages.js
+++ json/pages.js
@@ -2,23 +2,23 @@ const pages = [
   {
     name: 'hoofdpagina',
     text: 'Buitenspelen als toen voor de kinderen van nu!',
-    question: ' dit is een vraag van pagina 1',
+    question: '',
     imagePath: require('../images/logo.png'),
   },
   {
-    name: '1/6',
+    name: '1/7',
     text: "Zet de Picoo's aan door op de knop te drukken van elke controller.",
     question: "Als jij ze aan hebt gezet, knipperen de Picoo's nu blauw? ",
     imagePath: require('../images/pagetwo.png'),
   },
   {
-    name: '2/6',
+    name: '2/7',
     text: 'Zoek een spel uit en pak de gamekaart apart.',
     question: '',
     imagePath: require('../images/pagina3.png'),
   },
   {
-    name: '3/6',
+    name: '3/7',
     text: ' Houd nu de gamekaart onder de Picoo, zo weet de controller welk spel je gaat spelen.',
     information:
       'de controller heeft de gamekaart gescand als je geluidje hoort. Dit is nu je hoofdcontroller, deze blijft knipperen.',
@@ -26,22 +26,28 @@ const pages = [
     imagePath: require('../images/pagefour.png'),
   },
   {
-    name: '4/6',
+    name: '4/7',
     text: "Houd alle picoo's bij de hoofdcontroller (knipperende picoo).",
     question: ' Let goed op! \n' + 'Knippert  de hoofdcontroller nu wit licht?',
     imagePath: require('../images/pagefive.png'),
   },
   {
-    name: '5/6',
+    name: '5/7',
     text: 'Druk op de knop van de hoofdcontroller (knipperende Picoo) om het spel te starten.',
     question: ' begint het spel?',
     imagePath: require('../images/pagesix.png'),
   },
   {
-    name: '6/6',
+    name: '6/7',
     text: '',
     question:
       ' Goed gedaan! je weet nu hoe je Picoo opstart. Weet je al hoe het spel werkt?',
+    imagePath: require('../images/pagesix.png'),
+  },
+  {
+    name: '6/7',
+    text: '',
+    question: ' wow wat fucking fantastisch',
     imagePath: require('../images/pagesix.png'),
   },
 ];
